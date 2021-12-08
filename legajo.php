fdc366bbab030c89c9cfe0f11a50435343a1a34c
Ariel <arielanus1980@yahoo.com.ar>
2021-12-08 13:49:12 -0300

    ejercicioPHP

---
+++ legajo.php
@@ -0,0 +1,30 @@
+<?php
+
+$nombre = "Fernando";
+$apellido = "Lopez";
+$edad = 40;
+$hobbie = "Running";
+$editorDeCodigoFavorito = "Brackets";
+$sistemaOperativoUtilizado = "Windows";
+echo "El nombre es ".$nombre;
+echo "<br>";
+echo "<br>";
+echo "El apellido es ".$apellido;
+echo "<br>";
+echo "<br>";
+echo "La edad es de ".$edad." años";
+echo "<br>";
+echo "<br>";
+echo "Su hobbie preferido es ".$hobbie;
+echo "<br>";
+echo "<br>";
+echo "Edita codigo con ".$editorDeCodigoFavorito;
+echo "<br>";
+echo "<br>";
+echo "El sistema operativo que usa es ".$sistemaOperativoUtilizado;
+
+
+
+
+
+?>
